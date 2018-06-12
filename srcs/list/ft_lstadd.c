2e380d6aba733892ec0baf8b2f18c8ebd9ce80a3
Federico Floris <fzfloris@gmail.com>
2018-06-12 22:53:46 +0200

    Deleted useless instructions

--- srcs/list/ft_lstadd.c
+++ srcs/list/ft_lstadd.c
@@ -14,11 +14,8 @@
 
 void	ft_lstadd(t_list **alst, t_list *new)
 {
-	if (alst && *alst)
-	{
-		new->next = *alst;
-		*alst = new;
-	}
-	else if (alst)
-		*alst = new;
+	if (!alst)
+		return ;
+	new->next = *alst;
+	*alst = new;
 }
