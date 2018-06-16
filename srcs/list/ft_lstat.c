fde42e53cd2eb27ee6fc9c45f75b430fa0f40f48
fedefloris <fzfloris@gmail.com>
2018-06-16 18:28:14 +0200

    refactored ft_lstat.c

--- srcs/list/ft_lstat.c
+++ srcs/list/ft_lstat.c
@@ -6,7 +6,7 @@
 /*   By: ffloris <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 16:45:02 by ffloris           #+#    #+#             */
-/*   Updated: 2018/02/26 16:45:25 by ffloris          ###   ########.fr       */
+/*   Updated: 2018/06/16 18:25:56 by ffloris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,8 @@
 t_list		*ft_lstat(t_list *lst, size_t nbr)
 {
 	if (lst)
-	{
-		while (nbr > 0 && lst != NULL)
-		{
-			lst = lst->next;
-			nbr--;
-		}
-		return ((nbr == 0) ? lst : NULL);
-	}
-	return (NULL);
+		return (NULL);
+	while (nbr-- && lst != NULL)
+		lst = lst->next;
+	return ((!nbr) ? lst : NULL);
 }
