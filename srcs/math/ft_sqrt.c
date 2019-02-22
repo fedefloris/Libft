c70e584b87d73f4957baef70cd8f244f99e876ab
ffloris <fzfloris@gmail.com>
2019-02-22 11:58:05 +0100

    Norminette

--- srcs/math/ft_sqrt.c
+++ srcs/math/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: ffloris <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/09 09:56:30 by ffloris           #+#    #+#             */
-/*   Updated: 2018/11/14 17:59:50 by ffloris          ###   ########.fr       */
+/*   Updated: 2019/02/22 11:57:37 by ffloris          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ static double	abs_double(double a)
 
 double			ft_sqrt(double a)
 {
-	double epsilon;
-	double guess;
+	double		epsilon;
+	double		guess;
 
 	guess = a;
 	epsilon = 1e-15;
